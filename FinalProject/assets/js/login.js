1907c495241b9dd5190d0d597f540a1cb1eef7d5
khanimfarajzade <78DYCT6.code.edu.az>
2023-06-20 23:30:09 +0400

    add

--- FinalProject/assets/js/login.js
+++ FinalProject/assets/js/login.js
@@ -30,23 +30,3 @@ formMy.addEventListener("submit", function (e)  {
 });
 
 
-// const login_btn = document.querySelector(".login-btn");
-// const user = document.getElementById("user");
-// const pass = document.getElementById("pass");
-// let arr = [];
-// arr = JSON.parse(localStorage.getItem("Data"));
-
-// let result;
-
-// login_btn.addEventListener("click", () => {
-//   arr.forEach((element) => {
-//     result = user.value === element.name && pass.value === element.pass;
-//   });
-//   if (result) {
-//     window.location.href = "user_page.html";
-//   } else if (user.value === "admin" && pass.value === "admin123") {
-//     window.location.href = "admin.html";
-//   } else {
-//     alert("User not found");
-//   }
-// });
