7413b99a07441c35acf4876bd6189b8e74fdd089
khanimfarajzade <78DYCT6.code.edu.az>
2023-06-20 16:21:27 +0400

    add

---
+++ FinalProject/assets/js/addprd.js
@@ -0,0 +1,34 @@
+const url = document.querySelector("#url");
+const prdName = document.querySelector("#prdName");
+const price = document.querySelector("#price");
+const category = document.querySelector("#category");
+const description = document.querySelector("#description");
+const myForm = document.querySelector(".myForm");
+
+myForm.addEventListener("submit" ,function (e) {
+   
+    e.preventDefault;
+   fetch("http://localhost:3000/products" ,{
+    method: "POST",
+  body: JSON.stringify({
+   
+    name: prdName.value,
+    price: price.value,
+    category: category.value,
+    src: url.value,
+    description: description.value
+  }),
+  
+  headers: {
+    "Content-type": "application/json; charset=UTF-8"
+  }
+   })
+   .then(res =>res.json())
+   .then(
+    data =>{
+        console.log(data);
+    }
+   )
+  
+})
+
